317eb2102f4f3f84d75ecd5c4ce35406eec763bb
Abhiram Goud Bingi <bingiabhiramgoud2002@gmail.com>
2021-07-13 16:01:10 +0530

    Initial commit

--- first_project/Scripts/activate.bat
+++ first_project/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Desktop\backend\first_project\first_project
+set VIRTUAL_ENV=C:\Users\USER\desktop\backend\first_project\first_project
 
 if not defined PROMPT set PROMPT=$P$G
 
