88011816e1f4a5a967225561cd921c2721fc46fd
Abhiram Goud Bingi <bingiabhiramgoud2002@gmail.com>
2021-07-14 13:05:56 +0530

    “edit”

--- first_project/settings.py
+++ first_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2+k(j#9omze73n=b1k!%18@h)o3)$!6fu*il=!2o%0+le3l309'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'myalcherproject.herokuapp.com',
